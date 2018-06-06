a48d4c73a49477e3161843f128566aa44423282e
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2018-06-06 19:47:09 +0200

    More updates

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gitpullall
 A quick command that calls "git pull" on all subfolders in a directory
+
+There's not really much more to say about it, it just git pulls.
+
+# Install
+```
+[sudo] pip install gitpullall
+```
+
+# Preview
+![Image](https://i.alexflipnote.xyz/436056.gif)
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import re
+import codecs
 
 from setuptools import setup
 
@@ -12,6 +13,14 @@ if not version:
     raise RuntimeError('version is not set')
 
 
+try:
+    f = codecs.open('README.md', encoding='utf-8')
+    long_description = f.read()
+    f.close()
+except Exception as e:
+    long_description = ''
+
+
 setup(
     name='gitpullall',
     author='AlexFlipnote',
@@ -20,6 +29,7 @@ setup(
     packages=['gitpullall'],
     license='GNU v3',
     description='A Python module that calls "git pull" on all subfolders',
+    long_description=long_description,
     include_package_data=True,
     entry_points={
         'console_scripts': [
