3c2569e71d8ffc3ca5cc479b0fbe6914fc464585
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2018-06-06 19:53:33 +0200

    Better docs

--- README.md
+++ README.md
@@ -3,10 +3,16 @@ A quick command that calls "git pull" on all subfolders in a directory
 
 There's not really much more to say about it, it just git pulls.
 
-# Install
+## Install
+You can install this command with [Python](https://www.python.org/downloads/) using the included pip command (If you're on Windows, make sure you install to PATH in custom settings)
 ```
 [sudo] pip install gitpullall
 ```
 
-# Preview
+## Tested systems and works
+(If you've tested on other platforms and it works, please tell me)
+- Windows 10
+- Linux (Debian)
+
+## Preview
 ![Image](https://i.alexflipnote.xyz/436056.gif)
--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import re
-import codecs
 
 from setuptools import setup
 
@@ -13,14 +12,6 @@ if not version:
     raise RuntimeError('version is not set')
 
 
-try:
-    f = codecs.open('README.md', encoding='utf-8')
-    long_description = f.read()
-    f.close()
-except Exception as e:
-    long_description = ''
-
-
 setup(
     name='gitpullall',
     author='AlexFlipnote',
@@ -29,7 +20,6 @@ setup(
     packages=['gitpullall'],
     license='GNU v3',
     description='A Python module that calls "git pull" on all subfolders',
-    long_description=long_description,
     include_package_data=True,
     entry_points={
         'console_scripts': [
